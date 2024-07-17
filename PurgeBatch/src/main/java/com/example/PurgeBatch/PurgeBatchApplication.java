958fc3852b4a5b6aabd20a0613f3719d33efda21
sivakumar-s5 <sivakumar.a@springfive.com>
2024-07-17 23:23:20 +0530

    Add files via upload

---
+++ PurgeBatch/src/main/java/com/example/PurgeBatch/PurgeBatchApplication.java
@@ -0,0 +1,13 @@
+package com.example.PurgeBatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PurgeBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PurgeBatchApplication.class, args);
+	}
+
+}
---
+++ PurgeBatch/src/test/java/com/example/PurgeBatch/PurgeBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PurgeBatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PurgeBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
